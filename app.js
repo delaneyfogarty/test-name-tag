2e71b41812ac7f599739c981605fba6c6e31641e
Delaney Fogarty <delaney.fk@gmail.com>
2022-10-25 09:56:17 -0700

    created an update button function

--- app.js
+++ app.js
@@ -4,3 +4,10 @@ console.log('nameDisplay', nameDisplay);
 
 nameDisplay.textContent = 'Beau';
 nameDisplay.classList.add('green');
+
+const updateButton = document.getElementById('update-btn');
+updateButton.addEventListener('click', () => {
+    console.log('text');
+    const nameInput = document.getElementById('name-input');
+    nameDisplay.textContent = nameInput.value;
+});
